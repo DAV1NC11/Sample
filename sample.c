f12af5c471083548c93bf444de2f11a999e1c92b
DAV1NC11 <56307893+DAV1NC11@users.noreply.github.com>
2022-08-20 11:08:10 +0300

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Parrots Rams Kings\n" );
+    printf("3         5     2 \n");
+    printf("2         5     1 \n" );
+    printf("2         6     4");
+}
